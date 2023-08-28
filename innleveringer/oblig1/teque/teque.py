f776589d4b9461f2b461b27b7655540b3c7df7dd
Halvor Njåstad <halvorin@uio.no>
2023-08-28 14:25:54 +0200

    worked on linkedlist-version of teque.py

--- innleveringer/oblig1/teque/teque.py
+++ innleveringer/oblig1/teque/teque.py
@@ -1,13 +1,38 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.prev:Node = None
+        self.next:Node = None
 class Teque:
-    #todo write init
-    class Node:
-        def __init__(data):
-            this.data = data
-            this.prev = None
-            this.next = None
-        def next(): return this.next
-        def previous(): return this.prev
-        def data():return this.data
+    def __init__(self):
+        self.head : Node = None
+        self.middle : Node= None
+        self.tail : Node = None
+        self.size : Node= 0
+    def __init__(self, data):
+        self.head : Node= Node(data)
+        self.middle :Node = self.head
+        self.tail:Node= self.head
+        self.size:int = 1
     #todo write Teque methods
-    
+    def push_back(self, x):
+        newNode = Node(x)
+        self.tail.next = newNode
+        self.tail = newNode
+        size+=1
+    def push_front(self,x):
+        newNode = Node(x)
+        self.head.prev = newNode
+        self.head = newNode
+        size+=1
+    def push_middle(self, x):
+        newNode : Node = Node(x)
+        if self.size % 2 == 0: self.middle.next = newNode
+        else: self.middle.prev = newNode
+        self.middle = newNode
+        size+=1
+
+    def get(self, i:int) ->object:
+        #todo define 4 ranges and traverse based on these ranges
         
+        pass
