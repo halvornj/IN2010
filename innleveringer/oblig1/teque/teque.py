e82dd0ae9d786218e54530ffa4d7f59fe9e5fdd7
Halvor Njåstad <halvorin@uio.no>
2023-09-07 14:23:57 +0200

    deleted ll-teque implementation

--- innleveringer/oblig1/teque/teque.py
+++ innleveringer/oblig1/teque/teque.py
@@ -1,108 +1,30 @@
+from typing import Final
 import math
-#TODO: docstrings
 
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.prev:Node = None
-        self.next:Node = None
-
-    #todo: decorators to make nicer get- and set-methods.
-    def getPrev(self):return self.prev
-    def getNext(self): return self.next
-    def setPrev(self, n):self.prev = n
-    def setNext(self, n):self.next = n
-    def getData(self):return self.data
-
-    def __str__(self):
-        return str(self.prev.__class__) + ", "+self.data + ", "+str(self.next.__class__)
 class Teque:
+    
     def __init__(self):
-        self.head : Node = None
-        self.middle : Node= None
-        self.tail : Node = None
-        self.size : int= 0
-        self.middleSwitch :bool = False
+        self.array : list = []
 
-    def emptyPush(self, x):
-        newNode : Node = Node(x)
-        self.head = newNode
-        self.tail = newNode
-        self.middle = newNode
-        self.size +=1
 
-    #todo: push_back() and push_front() need to check if increase is 2, then middle needs to point back/ forward by 1
     def push_back(self, x):
-        if self.size == 0: return self.emptyPush(x)
-        newNode = Node(x)
-        newNode.setPrev(self.tail)
-        self.tail.setNext(newNode)
-        self.tail = newNode
-        self.size+=1
-    def push_front(self,x):
-        if self.size == 0: return self.emptyPush(x)
-        newNode = Node(x)
-        newNode.setNext(self.head)
-        self.head.setPrev(newNode)
-        self.head = newNode
-        self.size+=1
+        self.array.append(x)
+    def push_front(self, x):
+        self.array.insert(0, x)
     def push_middle(self, x):
-        if self.size == 0: return self.emptyPush(x)
-        newNode : Node = Node(x)
-        if self.size % 2 == 0: 
-            newNode.setPrev(self.middle)
-            newNode.setNext(self.middle.getNext())
-            self.middle.setNext(newNode)
-        else: 
-            newNode.setNext(self.middle)
-            newNode.setPrev(self.middle.getPrev())
-            self.middle.setPrev(newNode)
-        self.middle = newNode
-        self.size+=1
+        self.array.insert(math.ceil(len(self.array)/2), x)
 
-    """     
-    def get(self, i:int) ->object:
-        if i in range(0, math.floor(self.size/4)):
-            #trav forward from head
-            current : Node = self.head
-            for j in range(i):
-                current = current.getNext()
-            return current.getData()
-        elif i in range(math.floor(self.size/4), math.floor(self.size/2)):
-            #traverse back from mid
-            current : Node = self.middle
-            distance : int = math.floor(self.size /2)
-            for j in range(distance):
-                current = current.getPrev()
-            return current.getData()
-        elif i in range(math.floor(self.size/2), math.floor(self.size*(3/4))):
-            #traverse forward from mid
-            current : Node = self.middle
-            for j in range(math.floor(self.size/2)-i):
-                current = current.getNext()
-            return current.getData()
-        else:
-            #traverse back from tail
-            current : Node = self.tail
-            distance : int = self.size - i-1
-            for j in range(distance):
-                current = current.getPrev()
-            return current.getData()
- """
     def get(self, i:int):
-        current : Node = self.head
-        for j in range(i):
-            current = current.getNext()
-        return current.getData()
+        return self.array[i]
 
     def __str__(self):
-        printstr : str = "["
-        for i in range(len(self)):
-            printstr +=str(self.get(i))
-            printstr +=","
-        printstr = printstr[:-1]
-        printstr +="]"
-        return printstr
-    
+        outputStr: str = "["
+        for el in self.array:
+            outputStr += f"{str(el)}, "
+        outputStr = outputStr[:-1] + "]"
+        return outputStr
     def __len__(self):
-        return self.size
+        return len(self.array)
+    
+    #TODO __iter__() and __next__()
+
