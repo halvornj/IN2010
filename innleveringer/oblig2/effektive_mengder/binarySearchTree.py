95a9509a26b4924e647e8dbec2dac7dc80d8efdd
halvorin <halvorin@uio.no>
2023-09-29 14:08:56 +0200

    more on oblig2

--- innleveringer/oblig2/effektive_mengder/binarySearchTree.py
+++ innleveringer/oblig2/effektive_mengder/binarySearchTree.py
@@ -1,15 +1,29 @@
 from node import Node
+from typing import Generic
+from typing import TypeVar
 
+#TODO check that generic type T makes sense
+#TODO docstrings
 
-class BinarySearchTree:
+T = TypeVar('T')
+
+#*for litt mer forventet oppførsel, der klassen er mer black-box og andre programm ikke trenger holde styr på root og Noder osv, er implementasjonen modifisert for å kun t
+class BinarySearchTree(Generic(T)):
     def __init__(self):
         self.size:int = 0
         self.root:Node = None
 
-    def insert(self, node:Node, x:object) ->Node:
+    def insert(self, x:T):
+        return self.rec_insert(self.root, x)
 
+    def rec_insert(self, x:T, node:Node = None) ->Node:
         if node is None: node = Node(x)
         elif x < node.element: node.left = self.insert(node.left,x)
         elif x > node.element: node.right = self.insert(node.right, x)
         return node
     
+    def rec_search(self, node:Node, x:T):
+        if node == None:return None
+        if node.element == x:return node.element
+        if x < node.element: return self.rec_search(node.left, x)
+        if x> v.element:return self.rec_search(node.right, x)
